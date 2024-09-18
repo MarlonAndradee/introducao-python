ee4bffb2075a4e4972708c22137f8831b958ba94
Marlon <marlon.andrade.santos@gmail.com>
2024-09-18 18:17:01 -0300

    Primeiro teste git

---
+++ 3-8.py
@@ -0,0 +1,4 @@
+value_meter = int(input("Digite o valor em metros: "))
+milimeter_conversion = value_meter * 1000
+
+print(f"Convertendo metro para milímetro o resultado é {milimeter_conversion}mm")
